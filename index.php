6eb974932d8d82f7cca8cae8e390449246045b7e
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-06-04 16:13:09 +0200

    add img movies

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ include __DIR__ . '/movies.php';
     <h2>Movies</h2>
 
     <ul class="movie">
+        <li><img src="https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR1,0,182,268_AL_.jpg" alt="pulp fiction"></li>
         <li><span>Titolo:</span> <?php echo $movie1->title?></li>
         <li><span>Regista:</span> <?php echo $movie1->director?></li>
         <li><span>Lingua:</span> <?php echo $movie1->language?></li>
@@ -26,6 +27,7 @@ include __DIR__ . '/movies.php';
         <li><span>Prezzo Scontato del 20%:</span> <?php echo $movie1->discount(20)?> €</li>
     </ul>
     <ul class="movie">
+        <li><img src="https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg" alt="Il padrino"></li>
         <li><span>Titolo:</span> <?php echo $movie2->title?></li>
         <li><span>Regista:</span> <?php echo $movie2->director?></li>
         <li><span>Lingua:</span> <?php echo $movie2->language?></li>
@@ -38,6 +40,7 @@ include __DIR__ . '/movies.php';
         <li><span>Prezzo Scontato del 20%:</span> <?php echo $movie2->discount(20)?> €</li>
     </ul>
     <ul class="movie">
+        <li><img src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg" alt="le ali della libertà"></li>
         <li><span>Titolo:</span> <?php echo $movie3->title?></li>
         <li><span>Regista:</span> <?php echo $movie3->director?></li>
         <li><span>Lingua:</span> <?php echo $movie3->language?></li>
@@ -50,6 +53,7 @@ include __DIR__ . '/movies.php';
         <li><span>Prezzo Scontato del 20%:</span> <?php echo $movie3->discount(20)?> €</li>
     </ul>
     <ul class="movie">
+        <li><img src="https://m.media-amazon.com/images/M/MV5BMDdhODg0MjYtYzBiOS00ZmI5LWEwZGYtZDEyNDU4MmQyNzFkXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg" alt="apocalypse now"></li>
         <li><span>Titolo:</span> <?php echo $movie4->title?></li>
         <li><span>Regista:</span> <?php echo $movie4->director?></li>
         <li><span>Lingua:</span> <?php echo $movie4->language?></li>
@@ -62,6 +66,7 @@ include __DIR__ . '/movies.php';
         <li><span>Prezzo Scontato del 20%:</span> <?php echo $movie4->discount(20)?> €</li>
     </ul>
     <ul class="movie">
+        <li><img src="https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg" alt="ritorno al futuro"></li>
         <li><span>Titolo:</span> <?php echo $movie5->title?></li>
         <li><span>Regista:</span> <?php echo $movie5->director?></li>
         <li><span>Lingua:</span> <?php echo $movie5->language?></li>
